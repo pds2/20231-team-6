364f557b71c42f64aa1057bb017139ee207e2df4
TutiDiniz <131499201+TutiDiniz@users.noreply.github.com>
2023-06-17 18:57:27 -0300

    Terminei a pagina inicial pré login
    Agora falta criar os ambiente depois de logar como administrador ou como usuário.

--- src/sistema.cpp
+++ src/sistema.cpp
@@ -1,23 +1,34 @@
 #include "../include/sistema.hpp"
 
-Sistema::Sistema(){};
+Sistema::Sistema(){};// Carrega todas as informacoes DO banco de dados
 
-string Sistema::mostrarOpcoes(vector<string> opcoes)
-{
-  for (unsigned int i = 0; i < opcoes.size(); i++){
-    cout << i+1 << ". " << opcoes[i] << endl;
-  }
-
+string Sistema::mostrarOpcoes(string titulo, vector<string> opcoes){
   unsigned int escolha;
-  bool b = true;
+  bool flag = true;
 
+  while (flag){
+    if (titulo == "\tUSUARIO LOGADO: ") cout << titulo << _usuario_logado->getUsuario() << endl;
+    else if (titulo != "\n") cout << titulo << endl;
 
-  // Tratar excecoes
-  //while (b){
+    for (unsigned int i = 0; i < opcoes.size(); i++){
+    cout << i+1 << ". " << opcoes[i] << endl;
+    }
+
     cin >> escolha;
-  //b = false
-  //}
-
+    try{
+      if (escolha > opcoes.size() || escolha == 0){
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');        
+        throw invalid_argument("Escolha invalida!");
+      }
+      flag = false;
+    }
+    catch (invalid_argument &e){
+      cout << e.what() << endl;
+      sleep(1);
+      limparTela();
+    }
+  }
   cin.ignore(1000, '\n');
   return opcoes[escolha-1];
 }
@@ -25,9 +36,8 @@ string Sistema::mostrarOpcoes(vector<string> opcoes)
 void Sistema::paginaInicial()
 {
   while (true){
-    // Chamar uma funcao para carregar todas as informacoes DO banco de dados
     limparTela();
-    string opcao = mostrarOpcoes({"Fazer Login como Usuario", "Fazer Login como Administrador", "Cadastrar", "Fechar Programa"});
+    string opcao = mostrarOpcoes("\tPAGINA INICIAL", {"Fazer Login como Usuario", "Fazer Login como Administrador", "Cadastrar", "Fechar Programa"});
 
     if (opcao == "Fazer Login como Usuario"){
       do{
@@ -40,7 +50,7 @@ void Sistema::paginaInicial()
           opcao = "Voltar";
         }
         catch (erro_no_login_e &e){
-          opcao = mostrarOpcoes({"Tentar Novamente", "Voltar"});
+          opcao = mostrarOpcoes("\n", {"Tentar Novamente", "Voltar"});
         }
       } while (opcao != "Voltar");
     }
@@ -50,12 +60,14 @@ void Sistema::paginaInicial()
         limparTela();
         cout << "\tAREA DE LOGIN PARA ADMINISTRADORES" << endl;
         string senha = preencherString("Senha");
-        if (verificarAdmin(senha) == true){
-          // Chamar uma tela para administradores
+        if (verificaAdmin(senha) == true){
+          _admin_logado = new Admin();
+          paginaAdmin();
+          opcao = "Voltar";
         }
         else{
           cout << "Senha invalida!" << endl;
-          opcao = mostrarOpcoes({"Tentar Novamente", "Voltar"});
+          opcao = mostrarOpcoes("\n", {"Tentar Novamente", "Voltar"});
         }
       } while (opcao != "Voltar");
     }
@@ -63,6 +75,7 @@ void Sistema::paginaInicial()
     if (opcao == "Cadastrar"){
       do{
         limparTela();
+        cout << "\tAREA DE CADASTRO PARA USUARIOS" << endl;
         string usuario = preencherString("Usuario");
         try{
           verificarUsuario(usuario);
@@ -73,23 +86,23 @@ void Sistema::paginaInicial()
             
             cout << "Cadastro feito com sucesso! Bem vindo " << usuario << "!" << endl;
             sleep(2);
-            opcao = "Voltar";
             Conta* u1 = new Conta(usuario, senha);
             _usuarios.push_back(u1);
             logarUsuario(usuario, senha);
+            opcao = "Voltar";
           }
           catch (senha_invalida_e &e){
             cout << "Senha invalida!" << endl;
-            opcao = mostrarOpcoes({"Tentar Novamente", "Voltar"});
+            opcao = mostrarOpcoes("\n", {"Tentar Novamente", "Voltar"});
           }
           catch (senhas_diferentes_e &e){
             cout << "As senhas nao coincidem!" << endl;
-            opcao = mostrarOpcoes({"Tentar Novamente", "Voltar"});
+            opcao = mostrarOpcoes("\n", {"Tentar Novamente", "Voltar"});
           }
         }
         catch (usuario_ja_existe_e &e){
           cout << "Usuario ja existe!" << endl;
-          opcao = mostrarOpcoes({"Tentar Novamente", "Voltar"});
+          opcao = mostrarOpcoes("\n", {"Tentar Novamente", "Voltar"});
         }
       } while(opcao != "Voltar");
     }
@@ -101,7 +114,28 @@ void Sistema::paginaInicial()
   }
 }
 
-// Pronto
+// Criar um ambiente pos login
+void Sistema::paginaLogado(){
+  limparTela();
+  string opcao = mostrarOpcoes("\tUSUARIO LOGADO: ", {"Procurar Produto", "Listar Categorias", "Ver Carrinho", "Finalizar Compras", \
+   "Ver Compras Passadas", "Trocar Senha", "Deslogar"});
+  while (opcao != "Deslogar"){
+
+  }
+  _usuario_logado = nullptr;
+}
+
+// Cirar um ambiente para administradores
+void Sistema::paginaAdmin(){
+  limparTela();
+  string opcao = mostrarOpcoes("\tADMINISTRADOR LOGADO", {"Editar Produtos", "Edtiar Corredores", "Editar Usuarios", "Deslogar"});
+  while (opcao != "Deslogar"){
+
+  }
+  delete _admin_logado;
+  _admin_logado = nullptr;
+}
+
 string Sistema::preencherString(string campo){
   cout << campo << ": ";
   string entrada;
@@ -109,12 +143,11 @@ string Sistema::preencherString(string campo){
   return entrada;
 }
 
-// Pronto
 void Sistema::logarUsuario(const string& usuario, const string& senha){
   try{
     Conta* u1 = encontrarUsuario(usuario,senha);
     _usuario_logado = u1;
-    // Chamar uma tela para usuarios ja logados
+    paginaLogado();
   }
   catch (usuario_invalido_e &e){
     cout << "Usuario nao encontrado!" << endl;
@@ -126,7 +159,6 @@ void Sistema::logarUsuario(const string& usuario, const string& senha){
   }
 }
 
-// Pronto
 Conta* Sistema::encontrarUsuario(const string& usuario, const string& senha){
   for (Conta* c: _usuarios){
     if (c->getUsuario() == usuario){
@@ -148,7 +180,7 @@ bool Sistema::verificaAdmin(const string& senha){
 
 void Sistema::verificarUsuario(const string& usuario){
   for (Conta* c: _usuarios){
-    if (c->getUsuario() == usuario) throw usuario_ja_existe_e();
+    if (c->getUsuario() == usuario || usuario == "admin") throw usuario_ja_existe_e();
   }
 }
 
@@ -160,7 +192,6 @@ void Sistema::verificarSenhaCadastro(const string& senha, const string& senha_no
   if (senha != senha_novamente) throw senhas_diferentes_e();
 }
 
-// Pronto
 void Sistema::limparTela(){
   #ifdef _WIN32
     system("cls");
