6e36dab72a13fe46623286cbad55c95beb1b66bf
yanaquinoo <yanaquino88@gmail.com>
2023-06-18 14:21:51 -0300

    Novos atributos únicos para a classe

--- include/produto_limpeza.hpp
+++ include/produto_limpeza.hpp
@@ -7,11 +7,22 @@
 class ProdutoLimpeza : public Produto {
   private:
     std::string _aroma;
+    unsigned int _volume; // em ml
 
   public:
-    ProdutoLimpeza(const std::string &nome, double preco, int quantidade, const std::string &aroma);
+    // Construtor
+    ProdutoLimpeza(const std::string &nome, double preco, int quantidade,
+                   const std::string &aroma, unsigned int volume);
+
+    /*
+     * Imprime as informações do produto de limpeza através da função imprimir_informacoes.
+     * Além disso, imprime o aroma do produto e o volume.
+     */
     void imprimir_informacoes() override;
+
     std::string getAroma() const;
+
+    unsigned int getVolume() const;
 };
 
 #endif // PRODUTO_LIMPEZA_HPP
