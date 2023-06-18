f4fba5bf3fd9e820423dc723138858d93f4f1dd0
Caio <caio6776@gmail.com>
2023-06-18 00:00:45 -0300

    Mudei a classe Conta para criar as novas classes

--- src/conta.cpp
+++ src/conta.cpp
@@ -40,19 +40,6 @@ int callbackProdutos(void* data, int argc, char** argv, char** /* azColName */) 
     return 0;
 }
 
-// Função para recuperar os produtos comprados do banco de dados para uma conta específica
-std::vector<std::string> getProdutosCompradosFromDB(const std::string& usuario, sqlite3* db) {
-    std::vector<std::string> produtos;
-    std::string sql = "SELECT produto FROM produtos_comprados WHERE usuario = '" + usuario + "'";
-
-    int rc = sqlite3_exec(db, sql.c_str(), callbackProdutos, &produtos, nullptr);
-    if (rc != SQLITE_OK) {
-        std::cerr << "Erro ao executar a consulta SQL: " << sqlite3_errmsg(db) << std::endl;
-    }
-
-    return produtos;
-}
-
 Conta::Conta(const std::string& usuario, const std::string& senha) : usuario(usuario), senha(senha) {
     // Estabelecer conexão com o banco de dados
     sqlite3* db;
@@ -61,12 +48,6 @@ Conta::Conta(const std::string& usuario, const std::string& senha) : usuario(usu
         std::cerr << "Erro ao abrir o banco de dados: " << sqlite3_errmsg(db) << std::endl;
         // Lidar com o erro adequadamente
     }
-
-    // Recuperar os produtos comprados do banco de dados
-    produtosComprados = getProdutosCompradosFromDB(usuario, db);
-
-    // Fechar a conexão com o banco de dados
-    sqlite3_close(db);
 }
 
 std::string Conta::getUsuario() const {
@@ -76,39 +57,3 @@ std::string Conta::getUsuario() const {
 bool Conta::verificarSenha(const std::string& senha) const {
     return this->senha == senha;
 }
-
-void Conta::adicionarProdutoComprado(const std::string& produto) {
-    // Adicionar o produto à lista de produtos comprados
-    produtosComprados.push_back(produto);
-
-    // Atualizar o banco de dados com o novo produto comprado
-    sqlite3* db;
-    int rc = sqlite3_open("DATABASE.sqlite", &db);
-    if (rc != SQLITE_OK) {
-        std::cerr << "Erro ao abrir o banco de dados: " << sqlite3_errmsg(db) << std::endl;
-        // Lidar com o erro adequadamente
-    }
-
-    std::string sql = "INSERT INTO produtos_comprados (usuario, produto) VALUES ('" + usuario + "', '" + produto + "')";
-
-    rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, nullptr);
-    if (rc != SQLITE_OK) {
-        std::cerr << "Erro ao executar a consulta SQL: " << sqlite3_errmsg(db) << std::endl;
-        // Lidar com o erro adequadamente
-    }
-
-    sqlite3_close(db);
-}
-
-void Conta::exibirProdutosComprados() const {
-    std::cout << "Produtos comprados por " << usuario << ":" << std::endl;
-    for (const auto& produto : produtosComprados) {
-        std::cout << "- " << produto << std::endl;
-    }
-    
-}
-
-
-
-
-
