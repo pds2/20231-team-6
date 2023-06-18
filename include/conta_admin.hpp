f0cda30b2dc765983ef16339f1e9e25ce8f37656
Caio <caio6776@gmail.com>
2023-06-18 00:02:30 -0300

    class "Admin" (Conta) (não está implementada!!)

---
+++ include/conta_admin.hpp
@@ -0,0 +1,33 @@
+#ifndef ADMIN_HPP
+#define ADMIN_HPP
+
+#include "conta.hpp"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+    @brief Definição da classe admin (derivada de Conta) e seus métodos.
+ */
+class Admin : public Conta {
+public:
+    //Construtor de uma conta específica para um admin.
+    Admin(const std::string& usuario, const std::string& senha);
+
+    //Adiciona um ou mais produto(s) ao sistema.
+    void adicionarProdutos();
+
+    //Remove um ou mais produto(s) do sistema.
+    void removerProdutos();
+
+    //Cria novo desconto em um produto.
+    void addDescontoProduto();
+    
+    //Cria novo desconto em uma categoria/corredor de produtos.
+    void addDescontoCategoria();
+
+}
+
+#endif
