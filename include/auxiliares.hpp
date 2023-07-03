8a51f92118addf25e8fb2fda09beb0f08ee7621f
Caio <caio6776@gmail.com>
2023-07-03 16:33:51 -0300

    CRIEI UM ARQUIVO PARA FICAR SO COM AS FUNCOES AUX

---
+++ include/auxiliares.hpp
@@ -0,0 +1,50 @@
+#ifndef AUXILIARES_HPP
+#define AUXILIARES_HPP
+
+#include <exception>
+#include <cstdlib>
+#include <iostream>
+#include <unistd.h>
+#include <limits>
+#include <stdexcept>
+#include <vector>
+#include <typeinfo>
+
+/*
+* @brief Limpa a tela do console.
+*/
+void limparTelaA();
+
+/*
+* @brief Printa na tela todas as opções disponíveis para escolha do usuário,
+* bem como um título para a página (opcional). Retorna a opção escolhida.
+* @throws Lança uma exceção caso o usuário escolha uma opção inválida.
+*/
+std::string mostrarOpcoesA(std::string titulo, std::vector<std::string> opcoes, bool limpar);
+
+/*
+* @brief Printa na tela o que o usuário deve preencher e retorna a informação preenchida.
+*/
+std::string preencherStringA(std::string campo);
+
+/*
+* @brief Printa na tela o que o usuário deve preencher e retorna a informação preenchida.
+*/
+int preencherIntA(std::string campo);
+
+/*
+* @brief Printa na tela o que o usuário deve preencher e retorna a informação preenchida.
+*/
+double preencherDoubleA(std::string campo);
+
+/*
+* @brief Recebe uma string, que deve ser formatada de acordo com o padrão de pesquisa:
+* todas as palavras sempre começam com letra maiúscula.
+*/
+std::string stringPesquisaA(std::string entrada);
+
+//Vetor auxiliar com strings com o nome das subclasses.
+std::vector <std::string> _tipos_de_produtoA = {"Produto Genérico", "Produto Alimentício", "Produto de Limpeza", \
+"Produto Infantil", "Voltar"};
+
+#endif
