cf4af3d1d453f756702c058a9bb44562a9ed77e1
Caio <caio6776@gmail.com>
2023-07-03 16:34:12 -0300

    CRIEI FUNCOES AUX PARA LER INT E DOUBLE DO TECLADO

---
+++ src/auxiliares.cpp
@@ -0,0 +1,100 @@
+#include "auxiliares.hpp"
+
+void limparTelaA(){
+  #ifdef _WIN32
+    system("cls");
+  #else
+    system("clear");
+  #endif
+}
+
+std::string mostrarOpcoesA(std::string titulo, std::vector<std::string> opcoes, bool limpar){
+  unsigned int escolha;
+  bool flag = true;
+
+  while (flag){
+    for (unsigned int i = 0; i < opcoes.size(); i++){
+    std::cout << i+1 << ". " << opcoes[i] << std::endl;
+    }
+
+    std::cin >> escolha;
+    try{
+      if (escolha > opcoes.size() || escolha == 0){
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');        
+        throw std::invalid_argument("Escolha invalida!");
+      }
+      flag = false;
+    }
+    catch (std::invalid_argument &e){
+      std::cout << e.what() << std::endl;
+      sleep(1);
+      if (limpar){
+        limparTelaA();
+      }
+    }
+  }
+  std::cin.ignore(1000, '\n');
+  return opcoes[escolha-1];
+}
+
+std::string preencherStringA(std::string campo){
+  std::cout << campo << ": ";
+  std::string entrada;
+  getline(std::cin, entrada);
+  return entrada;
+}
+
+int preencherIntA(std::string campo){
+  int entrada = -1;
+  while (true) {
+    std::cout << campo << ": ";
+    std::cin >> entrada;
+    if (std::cin.fail()) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Entrada inválida. Por favor, digite um número válido.\n";
+    }
+    else if (entrada <= 0) {
+        std::cout << "O número deve ser maior que zero. Por favor, digite novamente.\n";
+    }
+    else {
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        break;
+    }
+  }
+  return entrada;
+}
+
+double preencherDoubleA(std::string campo) {
+    double entrada = -1.0;
+    while (true) {
+        std::cout << campo << ": ";
+        std::cin >> entrada;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Entrada inválida. Por favor, digite um número válido.\n";
+        }
+        else if (entrada < 0.0) {
+            std::cout << "O número deve ser maior que zero. Por favor, digite novamente.\n";
+        }
+        else {
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+        }
+    }
+    return entrada;
+}
+
+std::string stringPesquisaA(std::string entrada){
+  entrada[0] = toupper(entrada[0]);
+  for (unsigned int i = 1 ; i < entrada.size(); i++){
+    entrada[i] = tolower(entrada[i]);
+    if (entrada[i] == ' '){
+      entrada[i+1] = toupper(entrada[i+1]);
+      i++;
+    }
+  }
+  return entrada;
+}
