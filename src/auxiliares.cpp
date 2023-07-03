147f7ea1b34dee7a83e2d1c52a48735890ffaefe
Caio Santana <caio6776@gmail.com>
2023-07-03 20:41:58 -0300

    Revert "Revert "corrigindo bugs criar produto""
    This reverts commit d7f8558166dd0ad886f080478a76e68460f2c84d.

--- src/auxiliares.cpp
+++ src/auxiliares.cpp
@@ -11,7 +11,8 @@ void limparTela(){
 std::string mostrarOpcoesA(std::string titulo, std::vector<std::string> opcoes, bool limpar){
   unsigned int escolha;
   bool flag = true;
-
+  
+  std::cout << titulo << std::endl;
   while (flag){
     for (unsigned int i = 0; i < opcoes.size(); i++){
     std::cout << i+1 << ". " << opcoes[i] << std::endl;
