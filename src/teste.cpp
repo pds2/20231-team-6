9153d6c39e9e77e827da68b1e3aebf16783854dd
Caio <caio6776@gmail.com>
2023-07-05 02:52:32 -0300

    comentei

--- src/teste.cpp
+++ src/teste.cpp
@@ -1,31 +1,31 @@
 
 
 
-class ConstrutorProduto {
-    public:
-        virtual Produto *build_produto(outrosargs) = 0; 
-}
+// class ConstrutorProduto {
+//     public:
+//         virtual Produto *build_produto(outrosargs) = 0; 
+// }
 
-class ConstrutorArroz : public ConstrutorProduto {
-    public:
-        Produto *build_produto(outrosargs) override { return new Arroz };
-};
+// class ConstrutorArroz : public ConstrutorProduto {
+//     public:
+//         Produto *build_produto(outrosargs) override { return new Arroz };
+// };
 
 
-class ConstrutorSabao : public ConstrutorProduto {
-    public:
-        Produto *build_produto(outrosargs) override { return new Sabao };
-};
+// class ConstrutorSabao : public ConstrutorProduto {
+//     public:
+//         Produto *build_produto(outrosargs) override { return new Sabao };
+// };
 
-class ProdutoFactory {
-    private:
-        map<string, ConstrutorProduto *> construtores;
-    public:
-        ProdutoFactory() {
-            construtores["arroz"] = ConstrutorArroz();
-            construtores["sabao"] = ConstrutorSabao();
-        }
-        Produto *criar_produto(string nome, outrosargs) {
-            return construtores[nome].build_produto(outrosrgs);
-        }
-};
+// class ProdutoFactory {
+//     private:
+//         map<string, ConstrutorProduto *> construtores;
+//     public:
+//         ProdutoFactory() {
+//             construtores["arroz"] = ConstrutorArroz();
+//             construtores["sabao"] = ConstrutorSabao();
+//         }
+//         Produto *criar_produto(string nome, outrosargs) {
+//             return construtores[nome].build_produto(outrosrgs);
+//         }
+// };
