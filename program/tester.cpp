f0f3e5f29aaf616aef4706141db575f27e453a4c
Caio <caio6776@gmail.com>
2023-07-10 18:37:13 -0300

    testando construtores

--- program/tester.cpp
+++ program/tester.cpp
@@ -2,21 +2,51 @@
 #include "../third_party/doctest.h"
 #include "../include/sistema.hpp"
 
-TEST_CASE("01 - Testando Os Construtores da Classe Produtos") {
+TEST_CASE("01 - Testando O Construtor da Classe Produto") {
     Produto testeGenerico("generico", 15, 2);
-    CHECK(testeGenerico.getNome == "generico");
-    CHECK(testeGenerico.getPreco == 15);
-    CHECK(testeGenerico.getQuantidade == 2);
-    ProdutoAlimenticio testeAlimenticio("alimenticio", 100, 10, 0, "30/08/2023");
-    CHECK(testeAlimenticio.getNome == "alimenticio");
-    CHECK(testeAlimenticio.getPreco == 100);
-    CHECK(testeAlimenticio.getQuantidade == 10);
-    CHECK(testeAlimenticio.getVegano == 0);
-    CHECK(testeAlimenticio.getDataValidade == "30/08/2023");
-    //...
+    CHECK(testeGenerico.getNome() == "generico");
+    CHECK(testeGenerico.getPreco() == 15);
+    CHECK(testeGenerico.getQuantidade() == 2);
 }
 
-TEST_CASE("02 - Testando Os Construtores da Classe Conta") {
+TEST_CASE("02 - Testando O Construtor da Classe ProdutoAlimenticio") {
+    ProdutoAlimenticio testeAlimenticio("alimenticio", 100, 10, 0, 400, "30/08/2023");
+    CHECK(testeAlimenticio.getNome() == "alimenticio");
+    CHECK(testeAlimenticio.getPreco() == 100);
+    CHECK(testeAlimenticio.getQuantidade() == 10);
+    CHECK(testeAlimenticio.getVegano() == 0);
+    CHECK(testeAlimenticio.getPeso() == 400);
+    CHECK(testeAlimenticio.getDataValidade() == "30/08/2023");
+}
 
+TEST_CASE("03 - Testando O Construtor da Classe ProdutoLimpeza") {
+    ProdutoLimpeza testeLimpeza("limpeza", 23, 4, "lavanda", 500);
+    CHECK(testeLimpeza.getNome() == "limpeza");
+    CHECK(testeLimpeza.getPreco() == 23);
+    CHECK(testeLimpeza.getQuantidade() == 4);
+    CHECK(testeLimpeza.getAroma() == "lavanda");
+    CHECK(testeLimpeza.getVolume() == 500);
+}
+
+TEST_CASE("04 - Testando O Construtor da Classe ProdutoInfantil") {
+    ProdutoInfantil testeInfantil("infantil", 23, 4, "Para todos", 10);
+    CHECK(testeInfantil.getNome() == "limpeza");
+    CHECK(testeInfantil.getPreco() == 23);
+    CHECK(testeInfantil.getQuantidade() == 4);
+    CHECK(testeInfantil.getGenero() == "Para todos");
+    CHECK(testeInfantil.getIdade() == 10);
+}
+
+
+TEST_CASE("05 - Testando O Construtor da Classe Conta Admin (: Conta)") {
+    Admin testeAdmin("usuario", "senha");
+    CHECK(testeAdmin.getUsuario() == "usuario");
+    CHECK(testeAdmin.verificarSenha("senha") == 1);
+}
+
+TEST_CASE("06 - Testando O Construtor da Classe Consumidor (: Conta)") {
+    Consumidor testeConsumidor("usuario", "senha");
+    CHECK(testeConsumidor.getUsuario() == "usuario");
+    CHECK(testeConsumidor.verificarSenha("senha") == 1);
 }
 
